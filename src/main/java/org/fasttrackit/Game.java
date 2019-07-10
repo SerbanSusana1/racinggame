40790df5f589da863ea787c9ab08804f2021b004
susana.serban <susana.serban@casomes.ro>
2019-07-10 19:55:17 +0300

    studied Lists

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,18 +1,52 @@
 package org.fasttrackit;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Game {
 
-    private Vehicle competitor1;
-    private Vehicle competitor2;
+
     private Track[] tracks = new Track[3];
+
+    private List<Vehicle> competitors = new ArrayList<>();
 
 
     public void start() {
 
         initiliazeTracks();
         displayTracks();
+         int competitorCount=2;
+         for (int i=0; i<competitorCount; i++) {
+             addCompetitor();
+         }
+        displayCompetitors();
 
     }
+
+    private void addCompetitor() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Test");
+        vehicle.setFuelLevel(60);
+        vehicle.setMaxSpeed(200);
+        vehicle.setMileage(ThreadLocalRandom.current().nextDouble(4.5, 20));
+
+        competitors.add(vehicle);
+
+    }
+
+    private void displayCompetitors() {
+        System.out.println(("Today's competirors are :"));
+
+        for (int i = 0; i < competitors.size(); i++) {
+            if (competitors.get(i) != null) {
+                System.out.println(competitors.get(i).getName() + "- mileage:" + competitors.get(i).getMileage());
+            }
+        }
+
+    }
+
 
     private void initiliazeTracks() {
         Track city = new Track();
@@ -31,11 +65,12 @@ public class Game {
     private void displayTracks() {
         System.out.println("Available tracks: ");
         for (int i = 0; i < tracks.length; i++) {
-            if (tracks[i] !=null){
-             System.out.println((i + 1) + "." + tracks[i].getName());
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + "." + tracks[i].getName());
+            }
+
         }
 
     }
-
-}}
+}
 
